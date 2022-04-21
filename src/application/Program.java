ba1c50a0e9124334e216e4f5a0afc5e09fb858c8
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-04-20 21:44:21 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,5 +35,10 @@ public class Program {
 		{
 			System.out.println(sell);
 		}
+		
+		System.out.println("=== TEST 3: seller insert ===");
+		Seller newSeller = new Seller(null, "Lontra", "lontra@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
